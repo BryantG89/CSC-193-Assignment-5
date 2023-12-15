340ae9cdd140e98461136ce42d25ab34afed30da
BryantG <Bgalo89@gmail.com>
2023-12-15 12:49:53 -0800

    Updated button functions and added javascript file for button presses

---
+++ fancifymytext.js
@@ -0,0 +1,22 @@
+function biggerButton() {
+    alert("Hello, world!");
+}
+
+function applyStyles() {
+    var textArea = document.getElementById("textInputArea");
+    textArea.style.fontSize = "24pt";
+
+    var fancyShmancyRadioButton = document.getElementById("FancyShmancyChoice");
+    if (fancyShmancyRadioButton.checked) {
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    }
+}
+
+function removeStyles() {
+    var textArea = document.getElementById("textInput");
+    textArea.style.fontWeight = "normal";
+    textArea.style.color = "black";
+    textArea.style.textDecoration = "none";
+}
