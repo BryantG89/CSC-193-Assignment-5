828b6622560356a69996afc4bfa6a52a0abfead3
BryantG <Bgalo89@gmail.com>
2023-12-15 12:56:37 -0800

    Javascript code updated

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,5 +1,11 @@
-function biggerButton() {
+function biggerButton() 
+{
     alert("Hello, world!");
+}
+
+function sendAlert() 
+{
+    alert("Radio Button clicked");
 }
 
 function applyStyles() {
@@ -7,14 +13,16 @@ function applyStyles() {
     textArea.style.fontSize = "24pt";
 
     var fancyShmancyRadioButton = document.getElementById("FancyShmancyChoice");
-    if (fancyShmancyRadioButton.checked) {
+    if (fancyShmancyRadioButton.checked) 
+    {
         textArea.style.fontWeight = "bold";
         textArea.style.color = "blue";
         textArea.style.textDecoration = "underline";
     }
 }
 
-function removeStyles() {
+function removeStyles() 
+{
     var textArea = document.getElementById("textInput");
     textArea.style.fontWeight = "normal";
     textArea.style.color = "black";
