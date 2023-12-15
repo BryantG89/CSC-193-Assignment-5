2755606548b2154685560eb07d0c895232135c51
BryantG <Bgalo89@gmail.com>
2023-12-15 15:41:21 -0800

    Implemented and updated the moo button

--- fancifymytext.js
+++ fancifymytext.js
@@ -5,12 +5,35 @@ const boringBetty = document.getElementById("BoringBettyChoice");
 fancySchmancy.addEventListener("change", applyStyles);
 boringBetty.addEventListener("change", removeStyles);
 
-
+// Event Button for making input text size larger
 function biggerButton() 
 {
    document.getElementById("textInputArea").style.fontSize = "24px";
 }
 
+// Event Button for making "moo" string concatenation
+function mooButton()
+{
+    var textArea = document.getElementById("textInputArea");
+    var upperText = textArea.value.toUpperCase();
+    var sentences = upperText.split(/([.!?]+)/);
+
+    for (var i = 0; i < sentences.length; i += 2) 
+    {
+    var sentence = sentences[i];
+        if (/\s/.test(sentence)) 
+        {
+            var words = sentence.split(/\s+/);
+            words[words.length - 1] += "-Moo";
+            sentences[i] = words.join(" ");
+        }
+    }
+
+    var newtext = sentences.join("");
+    textArea.value = newtext;
+}
+
+// Event handler that alerts user when a font style has been picked
 function sendAlert() 
 {
     alert("A Font Style Was Chosen");
@@ -34,20 +57,9 @@ function applyStyles()
 function removeStyles() 
 {
     var textArea = document.getElementById("textInputArea");
-    textArea.style.fontSize = "12px";
+    textArea.style.fontSize = "13px";
     textArea.style.fontWeight = "normal";
     textArea.style.color = "black";
     textArea.style.textDecoration = "none";
 }
 
-// function fancifyOption() {
-//     document.getElementById("inputTextArea").style.color = "blue";
-//     document.getElementById("inputTextArea").style.fontWeight = "bold";
-//     document.getElementById("inputTextArea").style.textDecoration = "underline";
-//   }
-  
-//   function boringOption() {
-//     document.getElementById("myTextArea").style.color = "black";
-//     document.getElementById("myTextArea").style.fontWeight = "normal";
-//     document.getElementById("myTextArea").style.textDecoration = "none";
-//   }
